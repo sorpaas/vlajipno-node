0b669119c029c818f20c736a769090dd8f89db16
sorpa'as plat <sorpaas@gmail.com>
2014-05-16 10:02:02 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+{
+  "name": "vlajipno",
+  "version": "0.0.1",
+  "author": "sorpa'as plat <sorpaas@gmail.com>",
+  "description": "All text searching in Lojban.",
+  "main": "./lib/http-server",
+  "dependencies" : {
+    "express": "4.x"
+  },
+  "license": "MIT"
+}
